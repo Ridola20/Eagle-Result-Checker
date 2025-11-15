8d88f37b28617e0b999ccd52b3a236f9071b53c4
Ridola20 <ridwansanusiessential@gmail.com>
2025-11-15 06:25:24 +0100

    PWA

---
+++ static/sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'eagle-schools-v1.0.0';
+const urlsToCache = [
+  '/',
+  '/static/style.css',
+  '/static/logo.png',
+  '/static/icon-192x192.png',
+  '/static/icon-512x512.png',
+  '/static/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
